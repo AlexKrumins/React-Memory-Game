ab06acb6b03f447065f7ebc2b47353b1407cdb36
Alex Krumins <justkrumins@gmail.com>
2019-07-06 12:28:15 -0500

    changed delete function to 'checked' function

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    checked: [],
+  };
+
+  friendCheck = id => {
+    // Loop through the checked array to see if the id of the clicked character has ALREADY been clicked
+    //
+
+
+    
+    //When a character card is clicked, its id is added to the state.checked array.
+    this.setState({ checked: [...this.state.checked, id] })
+    console.log(this.state.checked)
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            friendCheck={this.friendCheck}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
