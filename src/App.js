dd024c34479801fc72469282bbcefd9f107c53b0
Alex Krumins <justkrumins@gmail.com>
2019-07-06 17:11:00 -0500

    scoreboard component working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
+import ScoreBoard from "./components/ScoreBoard";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
@@ -10,15 +11,14 @@ class App extends Component {
     friends,
     checked: [],
     score : 0,
+    topscore : 0
   };
   
   
   friendCheck = id => {
     // Loop through the checked array to see if the id of the clicked character has ALREADY been clicked
     this.state.checked.map(guess => {
-      if (guess === id) {
-        console.log("newGame()");
-      }
+      if (guess === id) return console.log("newGame()")
     })
     //This function will increase the user's score and 
     // when a character card is clicked, its id is added to the state.checked array.
@@ -30,12 +30,16 @@ class App extends Component {
   };
   
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title>Friends List</Title>
+        <ScoreBoard 
+        score={this.state.score}
+        topscore={this.state.topscore}
+        />
         {this.state.friends.map(friend => (
+        // Map over this.state.friends and render a FriendCard component for each friend object
           <FriendCard
             friendCheck={this.friendCheck}
             id={friend.id}
